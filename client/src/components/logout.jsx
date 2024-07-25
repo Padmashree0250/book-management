336ac9e5f76d31576d434740238c5efb7ae44dcf
Padmashree <128205619+Padmashree0250@users.noreply.github.com>
2024-07-25 13:53:49 +0530

    Update logout.jsx

--- client/src/components/logout.jsx
+++ client/src/components/logout.jsx
@@ -1,17 +1,1 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-const Logout = ({setRole})=>{
-    const navigate = useNavigate()
-   useEffect(()=>{
-   
-    .then(res=>{
-        if(res.data.logout){
-            setRole('')
-            navigate('/')
-        }
-    }).catch(err=> console.log(err))
-   },[])
-}
 
-export default Logout
