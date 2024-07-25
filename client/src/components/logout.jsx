efe7851d1ce5bbf6c7f0de7b42122a06c5e7a0f9
Padmashree <128205619+Padmashree0250@users.noreply.github.com>
2024-07-25 13:52:33 +0530

    Update logout.jsx

--- client/src/components/logout.jsx
+++ client/src/components/logout.jsx
@@ -4,8 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Logout = ({setRole})=>{
     const navigate = useNavigate()
    useEffect(()=>{
-    //axios.get('http://localhost:3001/auth/logout')   
-    axios.get('axios.get('http://localhost:3001/auth/logout')auth/logout') https://book-management-vsqt.vercel.app/
+   
     .then(res=>{
         if(res.data.logout){
             setRole('')
